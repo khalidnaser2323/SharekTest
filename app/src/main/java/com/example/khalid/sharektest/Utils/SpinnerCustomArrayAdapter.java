8e447774f7513fa308c309e58a339ed0f520775f
Mostafa-Fathi <mostafafathy677@gmail.com>
2016-09-23 20:04:13 +0200

    Adding classes

---
+++ app/src/main/java/com/example/khalid/sharektest/Utils/SpinnerCustomArrayAdapter.java
@@ -0,0 +1,38 @@
+package com.example.khalid.sharektest.Utils;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by User on 19/09/2016.
+ */
+public class SpinnerCustomArrayAdapter extends ArrayAdapter {
+    String one ;
+    public SpinnerCustomArrayAdapter(Context context, int resource) {
+    super(context, resource);
+}
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View v = super.getView(position, convertView, parent);
+        if (position == getCount()) {
+            ((TextView)v.findViewById(android.R.id.text1)).setText("");
+            ((TextView)v.findViewById(android.R.id.text1)).setHint((CharSequence) getItem(getCount())); //"Hint to be displayed"
+        }
+
+        return v;
+    }
+
+    @Override
+    public int getCount() {
+        return super.getCount()-1; // you dont display last item. It is used as hint.
+    }
+
+}
+
+
+
+
