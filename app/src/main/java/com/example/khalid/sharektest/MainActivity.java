2ce43e1d9ffcbc143f413eb1d176e65604ffd023
khalidnaser2323 <khalid.naser2323@gmail.com>
2016-10-18 00:01:44 +0200

    Revert "Revert "Sign up""
    This reverts commit f3682f1d2f6563d484cdc6c1791221be5041b327.

--- app/src/main/java/com/example/khalid/sharektest/MainActivity.java
+++ app/src/main/java/com/example/khalid/sharektest/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Log.i("Log","User is logged in");
             Intent intent=new Intent(this,HomePage.class);
             startActivity(intent);
+            // to skip login page
         }
 
 
